dfac1be0757f27852d3f6c13d962199fa82a241c
Vijay Krishnavanshi <vijaykrishnavanshi@gmail.com>
2019-08-23 23:58:26 +0530

    Added pre save hook and compare methods in schema

--- src/models/schema/user.js
+++ src/models/schema/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const { Schema } = mongoose;
 
 const _user = {};
@@ -7,6 +8,8 @@ _user.schema = new Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true },
+    password: { type: String, required: true },
+    salt: { type: String, required: true },
 
     // system generated
     createdAt: { type: Number, required: true },
@@ -15,6 +18,25 @@ _user.schema = new Schema(
   { usePushEach: true },
   { runSettersOnQuery: true },
 );
+
+_user.hashPassword = function hashPassword(password) {
+  const saltRounds = 5;
+  const salt = bcrypt.genSaltSync(saltRounds);
+  const hash = bcrypt.hashSync(password, salt);
+  return hash;
+};
+
+_user.schema.pre('save', function(next) {
+  const user = this;
+  // only hash the password if it has been modified (or is new)
+  if (!user.isModified('password')) return next();
+  user.password = _user.hashPassword(user.password);
+  next();
+});
+
+_user.schema.methods.comparePassword = function(candidatePassword) {
+  return bcrypt.compareSync(candidatePassword, this.password);
+};
 
 _user.model = mongoose.model('users', _user.schema);
 
