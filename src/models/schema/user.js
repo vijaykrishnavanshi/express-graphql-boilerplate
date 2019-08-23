a21a6a678e9a2199dbc4f2fbae6f8d2e15b60b81
Vijay Krishnavanshi <vijaykrishnavanshi@gmail.com>
2019-08-24 00:02:41 +0530

    Added cretaedAt and updatedAt to user collection

--- src/models/schema/user.js
+++ src/models/schema/user.js
@@ -9,10 +9,13 @@ _user.schema = new Schema(
     name: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
-    salt: { type: String, required: true },
 
     // system generated
-    createdAt: { type: Number, required: true },
+    createdAt: {
+      type: Number,
+      required: true,
+      default: Date.now,
+    },
     updatedAt: { type: Number },
   },
   { usePushEach: true },
@@ -30,6 +33,7 @@ _user.schema.pre('save', function(next) {
   const user = this;
   // only hash the password if it has been modified (or is new)
   if (!user.isModified('password')) return next();
+  user.updatedAt = Date.now();
   user.password = _user.hashPassword(user.password);
   next();
 });
