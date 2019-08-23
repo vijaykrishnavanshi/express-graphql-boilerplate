50f4a5a9894c57baac7f451d5f8841ff09ba2ae7
Vijay Krishnavanshi <vijaykrishnavanshi@gmail.com>
2019-08-24 00:56:56 +0530

    Minor Fix

--- src/graphql/user/Mutation/user_login.js
+++ src/graphql/user/Mutation/user_login.js
@@ -1,7 +1,6 @@
 const { UserController } = require('../../../controllers');
 
 const user_login = async (root, { email, password }) => {
-  console.log(email, password);
   const { token } = await UserController.login({ email, password });
   return { token };
 };
