15de35939056b554ab2a9df5038241db3e245db7
Vijay Krishnavanshi <vijaykrishnavanshi@gmail.com>
2019-08-24 04:14:28 +0530

    Updated Post_Update

--- src/graphql/post-module/Mutation/post_update.js
+++ src/graphql/post-module/Mutation/post_update.js
@@ -2,8 +2,7 @@ const { PostController } = require('../../../controllers');
 
 const post_update = async (root, { postId, postForm }) => {
   const post = await PostController.update(postId, {
-    title: postForm.title,
-    body: postForm.body,
+    ...postForm,
   });
   return post;
 };
