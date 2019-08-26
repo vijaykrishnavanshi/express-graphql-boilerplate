faadf25ff0c6f5d76f3eb656c25f353ebc1b3924
Vijay Krishnavanshi <vijaykrishnavanshi@gmail.com>
2019-08-27 01:00:28 +0530

    Fix path to config

--- config/index.js
+++ config/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 dotenv.config({
   path: path.resolve(
     __dirname,
-    `../${process.env.NODE_ENV ? process.env.NODE_ENV : ''}.env`,
+    `./${process.env.NODE_ENV ? process.env.NODE_ENV : ''}.env`,
   ),
 });
 const config = process.env;
