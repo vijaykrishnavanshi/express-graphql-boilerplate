d9ff7811eb494bbaa4fd4bb5ffa476a9e53fdd43
Vijay Krishnavanshi <vijaykrishnavanshi@gmail.com>
2019-08-27 00:36:27 +0530

    Added e2e tests to gql

---
+++ tests/e2e/graphql/user-module/user_signup.spec.js
@@ -0,0 +1,13 @@
+// eslint-disable-next-line node/no-unpublished-require
+const { expect } = require('chai');
+
+describe('mutation user_signup', async function() {
+  context('User Signup', async function() {
+    it('success case', async function() {
+      expect(true).to.equal(true);
+    });
+    it('failure case', async function() {
+      expect(true).to.equal(true);
+    });
+  });
+});
