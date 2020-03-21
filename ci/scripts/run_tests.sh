f48428d035111073454786f93b4d30c452a64e24
Vijay Krishnavanshi <vijaykrishnavanshi@gmail.com>
2020-03-22 05:01:13 +0530

    Run docker in docker for supporting services in ci

--- ci/scripts/run_tests.sh
+++ ci/scripts/run_tests.sh
@@ -1,10 +1,24 @@
 #!/usr/bin/env bash
 
-set -e -u -x
-wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
-apt-get update
-apt-get install -y mongodb-org
-systemctl start mongod
+source /docker-lib.sh
+start_docker
+# Strictly speaking, preloading of Docker images is not required.
+# However, you might want to do this for a couple of reasons:
+# - If the image comes from a private repository, it is much easier to let Concourse pull it,
+#   and then pass it through to the task.
+# - When the image is passed to the task, Concourse can often get the image from its cache.
+docker load -i mongo/image
+docker tag "$(cat mongo/image-id)" "$(cat mongo/repository):$(cat mongo/tag)"
+
+# This is just to visually check in the log that images have been loaded successfully.
+docker images
+
+# Run the container with tests and its dependencies.
+docker-compose -f express_gql/ci/services.yml up -d
 
 cd express_gql && npm install && npm test
+
+# Cleanup.
+# Not sure if this is required.
+# It's quite possible that Concourse is smart enough to clean up the Docker mess itself.
+docker volume rm $(docker volume ls -q)
