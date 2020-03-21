a9c35d26854af5f5bd01ba38d17ab210b5a9a52d
Vijay Krishnavanshi <vijaykrishnavanshi@gmail.com>
2020-03-22 04:16:16 +0530

    Update test script to setup mongodb before running tests

--- ci/scripts/run_tests.sh
+++ ci/scripts/run_tests.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
 set -e -u -x
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+apt-get update
+apt-get install -y mongodb-org
+systemctl start mongod
 
 cd express_gql && npm install && npm test
