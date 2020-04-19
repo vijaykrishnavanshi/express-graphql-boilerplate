17d8658a00fb7f9de4bf399fbab02a249dc25a42
Vijay Krishnavanshi <vijaykrishnavanshi@gmail.com>
2020-04-20 00:53:10 +0530

    Added generator for graphql scalar and directives

--- src/generator/index.js
+++ src/generator/index.js
@@ -2,10 +2,14 @@ const queryGenerator = require('./queryGenerator');
 const mutationGenerator = require('./mutationGenerator');
 const subscriptionGenerator = require('./subscriptionGenerator');
 const typeGenerator = require('./typeGenerator');
+const scalarGenerator = require('./scalarGenerator');
+const directiveGenerator = require('./directiveGenerator');
 
 module.exports = function (plop) {
   queryGenerator(plop);
   mutationGenerator(plop);
   subscriptionGenerator(plop);
   typeGenerator(plop);
+  scalarGenerator(plop);
+  directiveGenerator(plop);
 };
